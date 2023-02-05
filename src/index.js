5e9177fa89ce4a05d01abd6f5a4a6262f0eedbb4
平澤秀明 <hirasawahideaki@hirasawahideakinoMacBook-Pro.local>
2023-02-05 11:24:29 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import _ from 'lodash' 
+import _ from 'lodash'
+import { NAME as NAME_OF_HIDE } from './utilities';
+import tiger from './utilities';
+
+// console.log(utilities.NiJou(3));
+console.log(NAME_OF_HIDE);
+console.log(tiger.say());
 
 function component(){
     const element = document.createElement('div');
-    const array = ['Hello', 'webpack', '!'];
+    const array = ['Hello', 'webpack', '!!!'];
     element.innerHTML = _.join(array, ' ')
     return element;
 }
